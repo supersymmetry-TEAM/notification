03532c8eb6ae4fcf0335878b3bc39730099e4d73
supersymmetry-TEAM <62172128+supersymmetry-TEAM@users.noreply.github.com>
2020-05-18 11:31:39 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
-@app.route('/aitest')
+@app.route('/contributer')
 def aitest():
-    return render_template('ai_test.html')
+    return render_template('contributer.html')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/Privacy')
+def Pri():
+    return render_template('Privacy.html')
 
 if __name__== "__main__":
     app.run(host= '0.0.0.0',port ='5000')
+    #app.run(debug=True)
 
 
 
