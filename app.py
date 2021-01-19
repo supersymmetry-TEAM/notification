2eecbfd04973eee3af50e5861701fc1ac1ce8e3b
supersymmetry-TEAM <62172128+supersymmetry-TEAM@users.noreply.github.com>
2021-01-20 01:14:41 +0900

    Update : 2 image

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, redirect, send_file
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/0')
 def get_image():
-    name = "./source/gi.png"
+    name = "./source/gi0.png"
+    return send_file(name)
+@app.route('/1')
+def get_image():
+    name = "./source/gi1.png"
     return send_file(name)
 @app.route('/blog')
 def index():
