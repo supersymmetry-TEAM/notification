d993c04a378bdaaf1ccb7425734625140b50d390
supersymmetry-TEAM <62172128+supersymmetry-TEAM@users.noreply.github.com>
2021-01-20 01:46:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template, request, redirect, send_file
 app = Flask(__name__)
 
 @app.route('/1')
-def get_image():
+def get_image_1():
     name = "./source/1.png"
     return send_file(name)
 @app.route('/2')
-def get_image():
+def get_image_2():
     name = "./source/2.png"
     return send_file(name)
 @app.route('/blog')
